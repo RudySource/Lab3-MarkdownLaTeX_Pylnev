01d89535079090848288d5bbce90375119cf918a
Rud3us <rodionpylnev@yandex.ru>
2026-01-18 21:46:29 +0300

    Completed README.md with full Markdown and LaTeX showcase

---
+++ Program/FormatDemo.cs
@@ -0,0 +1,14 @@
+﻿namespace Program;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите 1 число: ");
+        double first = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите 2 число: ");
+        double second = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"**Результат: {first + second}**");
+
+    }
+}
